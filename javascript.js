2b253d36b79b688e9bd0ba981bf7524902bf72c9
Sa1ram06 <s10259930@connect.np.edu.sg>
2024-01-30 14:42:09 +0800

    index page done

--- javascript.js
+++ javascript.js
@@ -1,3 +1,73 @@
+const openPopButtons = document.querySelectorAll('[data-popup-target]');
+const closePopButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+const popupImage = document.querySelector('#pop_up_image');
+const popupTitle = document.querySelector('.quiz_name');
+const startQuizButton = document.querySelector('.start-quiz');
+
+
+openPopButtons.forEach(button=>{
+    button.addEventListener('click',() =>{
+        const popup = document.querySelector(button.dataset.popupTarget)
+        const quizTitle = button.dataset.quizTitle;
+        const quizImage = button.dataset.quizImage;
+    
+            // Set the content for the popup
+        popupTitle.textContent = quizTitle;
+        popupImage.src = quizImage;
+        openpopup(popup)
+    })
+})
+
+closePopButtons.forEach(button=>{
+    button.addEventListener('click',() =>{
+        const popup = button.closest('.pop-up')
+        closepopup(popup)
+    })
+})
+function openpopup(modal){
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+function closepopup(modal){
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+overlay.addEventListener('click',() =>{
+    const modals =document.querySelectorAll('.pop-up.active')
+    modals.forEach(modal => {
+        closepopup(modal)
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 1 Set  and intilaise variable
 var questionCount = 0;
 var score = 0;
