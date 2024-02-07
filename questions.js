3a4b03a9fdb201188e1e44113604aad555e5d72f
Sa1ram06 <s10259930@connect.np.edu.sg>
2024-02-08 07:10:15 +0800

    Updated all questions for all quizzes

--- questions.js
+++ questions.js
@@ -1,15 +1,16 @@
 const quizData = [
+  /* Grammer Questions */
   {
-      "Quiz":"Grammar",
-      "number": 1,
-      "question": "What is a noun?",
-      "answer":"A person, place, thing, or idea",
-      "option": [
-                "A person, place, thing, or idea",
-                "A type of verb",
-                "A punctuation mark",
-                "A unit of measurement"
-                ]
+    "Quiz":"Grammar",
+    "number": 1,
+    "question": "What is a noun?",
+    "answer":"A person, place, thing, or idea",
+    "option": [
+      "A person, place, thing, or idea",
+      "A type of verb",
+      "A punctuation mark",
+      "A unit of measurement"
+      ]
       
   },
   {
@@ -120,6 +121,7 @@ const quizData = [
       "Ran"
     ]
   },
+/* Vocab Questions */
   {
     "Quiz": "Vocabulary",
     "number": 1,
@@ -139,9 +141,9 @@ const quizData = [
     "answer": "No longer in use; outdated",
     "option": [
       "Modern",
+      "Brand new",
       "No longer in use; outdated",
-      "Functional",
-      "Brand new"
+      "Functional"
     ]
   },
   {
@@ -150,8 +152,8 @@ const quizData = [
     "question": "Provide a synonym for 'enthusiastic'.",
     "answer": "Eager, passionate",
     "option": [
-      "Lazy",
       "Eager, passionate",
+      "Lazy",
       "Tired",
       "Bored"
     ]
@@ -162,9 +164,9 @@ const quizData = [
     "question": "What is the meaning of 'contribute'?",
     "answer": "To give or add to something",
     "option": [
+      "To divide",
       "To subtract",
       "To give or add to something",
-      "To divide",
       "To isolate"
     ]
   },
@@ -187,9 +189,9 @@ const quizData = [
     "question": "Define 'persistent'.",
     "answer": "Continuing firmly or obstinately in a course of action",
     "option": [
-      "Changing",
       "Continuing firmly or obstinately in a course of action",
       "Weak",
+      "Changing",
       "Random"
     ]
   },
@@ -212,9 +214,9 @@ const quizData = [
     "answer": "Capable of recovering quickly from difficulties",
     "option": [
       "Fragile",
-      "Capable of recovering quickly from difficulties",
       "Stagnant",
-      "Old"
+      "Old",
+      "Capable of recovering quickly from difficulties",
     ]
   },
   {
@@ -236,19 +238,822 @@ const quizData = [
     "answer": "To assess or judge the value or importance of something",
     "option": [
       "To ignore",
-      "To assess or judge the value or importance of something",
       "To create",
+      "To assess or judge the value or importance of something",
       "To finish"
     ]
+  },
+  /* Spelling bEE Questions */
+  {
+    "Quiz": "Spelling Bee",
+    "number": 1,
+    "question": "Unscramble the letters 'etateleiv' to form a word.",
+    "answer": "levitate",
+    "option": [
+      "evetilate",
+      "televatie",
+      "eliteavet",
+      "levitate"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 2,
+    "question": "Unscramble the letters 'garmmea' to form a word.",
+    "answer": "grammar",
+    "option": [
+      "grammar",
+      "garagem",
+      "gammare",
+      "grammare"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 3,
+    "question": "Unscramble the letters 'inofnartmio' to form a word.",
+    "answer": "information",
+    "option": [
+      "formation",
+      "information",
+      "firmation",
+      "informtion"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 4,
+    "question": "Unscramble the letters 'voicder' to form a word.",
+    "answer": "discovery",
+    "option": [
+      "covdriey",
+      "discovery",
+      "derycivo",
+      "cdivoyer"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 5,
+    "question": "Unscramble the letters 'cericnfoe' to form a word.",
+    "answer": "conference",
+    "option": [
+      "confierenc",
+      "confercien",
+      "conference",
+      "confrencie"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 6,
+    "question": "Unscramble the letters 'etqursoi' to form a word.",
+    "answer": "question",
+    "option": [
+      "requestio",
+      "inquires",
+      "questions",
+      "question"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 7,
+    "question": "Unscramble the letters 'steionclia' to form a word.",
+    "answer": "elocinatis",
+    "option": [
+      "instelicao",
+      "socialtien",
+      "elocinatis",
+      "instaicleo"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 8,
+    "question": "Unscramble the letters 'eptarilv' to form a word.",
+    "answer": "prevital",
+    "option": [
+      "parvitle",
+      "private",
+      "prevital",
+      "viraplet"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 9,
+    "question": "Unscramble the letters 'oncintsiet' to form a word.",
+    "answer": "conscient",
+    "option": [
+      "coetnnisti",
+      "conscient",
+      "connistiet",
+      "scientonic"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 10,
+    "question": "Unscramble the letters 'crbiliaity' to form a word.",
+    "answer": "calibirty",
+    "option": [
+      "briliacty",
+      "cralibity",
+      "calibirty",
+      "clarity"
+    ]
+  },
+  {
+    "Quiz": "Spelling Bee",
+    "number": 10,
+    "question": "Unscramble the letters 'crbiliaity' to form a word.",
+    "answer": "calibirty",
+    "option": [
+      "briliacty",
+      "cralibity",
+      "calibirty",
+      "clarity"
+    ]
+  },
+  c
+  {
+    "Quiz": "Addition",
+    "number": 1,
+    "question": "What is 789 + 432?",
+    "answer": "1221",
+    "option": [
+      "1211",
+      "1112",
+      "1221",
+      "1122"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 2,
+    "question": "What is 9876 + 5432?",
+    "answer": "15308",
+    "option": [
+      "15432",
+      "15308",
+      "15280",
+      "15520"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 3,
+    "question": "What is 12345 + 67890?",
+    "answer": "80235",
+    "option": [
+      "80135",
+      "80025",
+      "80235",
+      "80325"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 4,
+    "question": "What is 876543 + 210987?",
+    "answer": "1087530",
+    "option": [
+      "1085760",
+      "1087530",
+      "1083705",
+      "1095307"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 5,
+    "question": "What is 123456 + 654321?",
+    "answer": "777777",
+    "option": [
+      "777777",
+      "777888",
+      "766666",
+      "788888"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 6,
+    "question": "What is 987654 + 123456?",
+    "answer": "1111110",
+    "option": [
+      "1111110",
+      "1122220",
+      "1100011",
+      "1111101"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 7,
+    "question": "What is 543210 + 876543?",
+    "answer": "1419753",
+    "option": [
+      "1417953",
+      "1409753",
+      "1419753",
+      "1417359"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 8,
+    "question": "What is 9876543 + 1234567?",
+    "answer": "11111110",
+    "option": [
+      "11111011",
+      "11111000",
+      "11111110",
+      "11111001"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 9,
+    "question": "What is 13579 + 24680?",
+    "answer": "38259",
+    "option": [
+      "38259",
+      "38269",
+      "39259",
+      "38258"
+    ]
+  },
+  {
+    "Quiz": "Addition",
+    "number": 10,
+    "question": "What is 24680 + 13579?",
+    "answer": "38259",
+    "option": [
+      "38269",
+      "38259",
+      "39259",
+      "38258"
+    ]
+  },
+/* Subtraction Questions */
+  {
+    "Quiz": "Subtraction",
+    "number": 1,
+    "question": "What is 789 - 432?",
+    "answer": "357",
+    "option": [
+      "357",
+      "345",
+      "447",
+      "322"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 2,
+    "question": "What is 9876 - 5432?",
+    "answer": "4444",
+    "option": [
+      "4321",
+      "4444",
+      "5344",
+      "5566"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 3,
+    "question": "What is 12345 - 6789?",
+    "answer": "5556",
+    "option": [
+      "5556",
+      "5467",
+      "5656",
+      "5678"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 4,
+    "question": "What is 876543 - 210987?",
+    "answer": "665556",
+    "option": [
+      "655556",
+      "676556",
+      "665556",
+      "654556"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 5,
+    "question": "What is 123456 - 65432?",
+    "answer": "58024",
+    "option": [
+      "57024",
+      "59024",
+      "58024",
+      "56024"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 6,
+    "question": "What is 987654 - 123456?",
+    "answer": "864198",
+    "option": [
+      "875198",
+      "864198",
+      "853198",
+      "875298"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 7,
+    "question": "What is 543210 - 87654?",
+    "answer": "455556",
+    "option": [
+      "455556",
+      "465556",
+      "445556",
+      "465656"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 8,
+    "question": "What is 9876543 - 1234567?",
+    "answer": "8641976",
+    "option": [
+      "8642976",
+      "8641976",
+      "8741976",
+      "8741876"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 9,
+    "question": "What is 13579 - 2468?",
+    "answer": "11111",
+    "option": [
+      "11211",
+      "11011",
+      "11111",
+      "11311"
+    ]
+  },
+  {
+    "Quiz": "Subtraction",
+    "number": 10,
+    "question": "What is 24680 - 13579?",
+    "answer": "11101",
+    "option": [
+      "11201",
+      "11301",
+      "11101",
+      "11001"
+    ]
+  },
+/* Multiplication Questions */
+  {
+    "Quiz": "Multiplication",
+    "number": 1,
+    "question": "What is 7 x 9?",
+    "answer": "63",
+    "option": [
+      "54",
+      "63",
+      "72",
+      "81"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 2,
+    "question": "What is 12 x 8?",
+    "answer": "96",
+    "option": [
+      "88",
+      "96",
+      "104",
+      "112"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 3,
+    "question": "What is 15 x 6?",
+    "answer": "90",
+    "option": [
+      "84",
+      "96",
+      "102",
+      "90"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 4,
+    "question": "What is 9 x 11?",
+    "answer": "99",
+    "option": [
+      "99",
+      "88",
+      "110",
+      "121"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 5,
+    "question": "What is 16 x 7?",
+    "answer": "112",
+    "option": [
+      "105",
+      "112",
+      "119",
+      "126"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 6,
+    "question": "What is 14 x 5?",
+    "answer": "70",
+    "option": [
+      "65",
+      "75",
+      "70",
+      "80"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 7,
+    "question": "What is 11 x 10?",
+    "answer": "110",
+    "option": [
+      "99",
+      "105",
+      "110",
+      "121"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 8,
+    "question": "What is 13 x 9?",
+    "answer": "117",
+    "option": [
+      "104",
+      "117",
+      "130",
+      "143"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 9,
+    "question": "What is 18 x 4?",
+    "answer": "72",
+    "option": [
+      "72",
+      "64",
+      "80",
+      "88"
+    ]
+  },
+  {
+    "Quiz": "Multiplication",
+    "number": 10,
+    "question": "What is 20 x 3?",
+    "answer": "60",
+    "option": [
+      "55",
+      "65",
+      "70",
+      "60"
+    ]
+  },
+  /* Living Things Questions */
+  {
+    "Quiz": "Living Things",
+    "number": 1,
+    "question": "What is the process by which living organisms produce energy from food?",
+    "answer": "Respiration",
+    "option": [
+      "Photosynthesis",
+      "Digestion",
+      "Respiration",
+      "Metabolism"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 2,
+    "question": "What is the process by which green plants use sunlight to synthesize food from carbon dioxide and water?",
+    "answer": "photosynthesis",
+    "option": [
+      "respiration",
+      "digestion",
+      "photosynthesis",
+      "metabolism"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 3,
+    "question": "Which organelle is known as the 'powerhouse' of the cell because it produces energy?",
+    "answer": "mitochondria",
+    "option": [
+      "nucleus",
+      "chloroplast",
+      "mitochondria",
+      "ribosome"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 4,
+    "question": "What is the basic structural and functional unit of all living organisms?",
+    "answer": "cell",
+    "option": [
+      "tissue",
+      "organ",
+      "cell",
+      "organism"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 5,
+    "question": "What is the green pigment found in plants that is essential for photosynthesis?",
+    "answer": "chlorophyll",
+    "option": [
+      "melanin",
+      "hemoglobin",
+      "chlorophyll",
+      "carotene"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 6,
+    "question": "What is the process by which organisms break down organic matter and convert it into energy?",
+    "answer": "metabolism",
+    "option": [
+      "respiration",
+      "digestion",
+      "photosynthesis",
+      "metabolism"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 7,
+    "question": "Which kingdom of living organisms includes multicellular organisms that lack cell walls, can move voluntarily, and usually ingest food?",
+    "answer": "Animalia",
+    "option": [
+      "Plantae",
+      "Animalia",
+      "Fungi",
+      "Protista"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 8,
+    "question": "Which domain of living organisms includes all prokaryotic organisms?",
+    "answer": "Bacteria",
+    "option": [
+      "Bacteria",
+      "Archaea",
+      "Eukarya",
+      "Protozoa"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 9,
+    "question": "What is the process by which living organisms take in oxygen and release carbon dioxide?",
+    "answer": "respiration",
+    "option": [
+      "photosynthesis",
+      "digestion",
+      "respiration",
+      "metabolism"
+    ]
+  },
+  {
+    "Quiz": "Living Things",
+    "number": 10,
+    "question": "What is the process by which living organisms break down food into simpler substances for energy?",
+    "answer": "digestion",
+    "option": [
+      "photosynthesis",
+      "respiration",
+      "digestion",
+      "metabolism"
+    ]
+  },
+/* Plants Questions */
+  {
+    "Quiz": "Plants",
+    "number": 1,
+    "question": "What is the process by which plants lose water vapor through small openings in their leaves?",
+    "answer": "transpiration",
+    "option": [
+      "photosynthesis",
+      "respiration",
+      "transpiration",
+      "pollination"
+    ]
+  },
+  {
+    "Quiz": "Plants",
+    "number": 2,
+    "question": "What is the tissue in plants responsible for transporting water and minerals from the roots to the rest of the plant?",
+    "answer": "xylem",
+    "option": [
+      "phloem",
+      "xylem",
+      "epidermis",
+      "mesophyll"
+    ]
+  },
+  {
+    "Quiz": "Plants",
+    "number": 3,
+    "question": "Which gas is absorbed by plants during photosynthesis and released during respiration?",
+    "answer": "carbon dioxide",
+    "option": [
+      "oxygen",
+      "carbon dioxide",
+      "nitrogen",
+      "water vapor"
+    ]
+  },
+  {
+    "Quiz": "Plants",
+    "number": 4,
+    "question": "Which part of the plant anchors it to the soil and absorbs water and minerals?",
+    "answer": "root",
+    "option": [
+      "stem",
+      "leaf",
+      "root",
+      "flower"
+    ]
+  },
+  {
+    "Quiz": "Plants",
+    "number": 5,
+    "question": "What is the male reproductive organ of a flower called?",
+    "answer": "stamen",
+    "option": [
+      "pistil",
+      "stigma",
+      "stamen",
+      "petal"
+    ]
+  },
+  {
+    "Quiz": "Plants",
+    "number": 6,
+    "question": "Which process involves the transfer of pollen grains from the anther to the stigma of a flower?",
+    "answer": "pollination",
+    "option": [
+      "fertilization",
+      "germination",
+      "pollination",
+      "reproduction"
+    ]
+  },
+  {
+    "Quiz": "Plants",
+    "number": 7,
+    "question": "What is the process by which plants convert light energy into chemical energy to produce glucose?",
+    "answer": "photosynthesis",
+    "option": [
+      "respiration",
+      "transpiration",
+      "photosynthesis",
+      "pollination"
+    ]
+  },
+  {
+    "Quiz": "Plants",
+    "number": 8,
+    "question": "Which tissue in plants is responsible for transporting sugars produced during photosynthesis to various parts of the plant?",
+    "answer": "phloem",
+    "option": [
+      "xylem",
+      "phloem",
+      "epidermis",
+      "mesophyll"
+    ]
+  },
+  /* Magnets Questions */
+  {
+    "Quiz": "Magnets",
+    "number": 1,
+    "question": "What is the property of a magnet that allows it to attract objects made of iron, nickel, or cobalt?",
+    "answer": "magnetic force",
+    "option": [
+      "gravitational force",
+      "magnetic force",
+      "electric force",
+      "frictional force"
+    ]
+  },
+  {
+    "Quiz": "Magnets",
+    "number": 2,
+    "question": "What is the region around a magnet where its magnetic force is felt?",
+    "answer": "magnetic field",
+    "option": [
+      "magnetic region",
+      "magnetic zone",
+      "magnetic field",
+      "magnetic space"
+    ]
+  },
+  {
+    "Quiz": "Magnets",
+    "number": 3,
+    "question": "Which type of magnet retains its magnetism after being removed from a magnetic field?",
+    "answer": "permanent magnet",
+    "option": [
+      "temporary magnet",
+      "permanent magnet",
+      "electromagnet",
+      "ferromagnet"
+    ]
+  },
+  {
+    "Quiz": "Magnets",
+    "number": 4,
+    "question": "What is the name of the material that is attracted to a magnet and can become magnetized itself?",
+    "answer": "ferromagnetic material",
+    "option": [
+      "non-magnetic material",
+      "magnetic material",
+      "paramagnetic material",
+      "ferromagnetic material"
+    ]
+  },
+  {
+    "Quiz": "Magnets",
+    "number": 5,
+    "question": "Which pole of a magnet points towards the Earth's geographic North Pole?",
+    "answer": "north pole",
+    "option": [
+      "south pole",
+      "north pole",
+      "east pole",
+      "west pole"
+    ]
+  },
+  {
+    "Quiz": "Magnets",
+    "number": 6,
+    "question": "What happens when opposite poles of two magnets are brought close together?",
+    "answer": "they attract each other",
+    "option": [
+      "they attract each other",
+      "they repel each other",
+      "nothing happens",
+      "they align with each other"
+    ]
+  },
+  {
+    "Quiz": "Magnets",
+    "number": 7,
+    "question": "What happens when like poles of two magnets are brought close together?",
+    "answer": "they repel each other",
+    "option": [
+      "they attract each other",
+      "they repel each other",
+      "nothing happens",
+      "they align with each other"
+    ]
+  },
+  {
+    "Quiz": "Magnets",
+    "number": 8,
+    "question": "Which type of magnet can be turned on and off by controlling the flow of electricity?",
+    "answer": "electromagnet",
+    "option": [
+      "permanent magnet",
+      "temporary magnet",
+      "ferromagnet",
+      "electromagnet"
+    ]
   }
-  
-  
-  
-  
-  
-  
-  
-  
 ]
 
 
